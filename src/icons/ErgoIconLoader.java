c25b587099a0ee801562d0d703675454feac761b
Ross <rosssullivan101@gmail.com>
2018-02-02 09:21:13 -0600

    Deleted unnessary files

--- src/icons/ErgoIconLoader.java
+++
@@ -1,9 +0,0 @@
-package Icons;
-
-import com.intellij.openapi.util.IconLoader;
-import javax.swing.*;
-
-public interface ErgoIconLoader{
-    Icon ERGO_PLAY_BUTTON = IconLoader.getIcon("/icons/button_image16.png");
-    Icon ERGO_PLAY_BUTTON_LOADING = IconLoader.getIcon("/icons/button_image16_dark.png");
-}
